79acbaa32cec846706dcb0759b0fa4d91af33805
Barun Nafak <bnafak@gmail.com>
2023-09-20 11:50:49 +0600

    upto  module 45-7 done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
@@ -6,8 +6,10 @@ const Header = () => {
             <nav>
             
                 <span>My Website</span>
+                <h1>Module 45-8 pending</h1>
                 <Link to="/">Home</Link>
-               <Link to="/users">Users</Link>
+                <NavLink to="/users">Users</NavLink>
+               {/* <Link to="/users">Users</Link> */}
                <Link to="/posts">Posts:</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact Us</Link>
--- src/Components/PostDetails/PostDetails.jsx
+++ src/Components/PostDetails/PostDetails.jsx
@@ -1,15 +1,25 @@
 
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const PostDetails = () => {
     const post = useLoaderData();
+    const {postId} = useParams();
+    const navigate = useNavigate();
     const {id, title, body} = post;
+
+    console.log(postId);
+
+    const handleGoback =() => {
+        navigate(-1);
+    }
+
     return (
         <div>
-            <h3>Post details about:</h3>
+            <h3>Post details about:{id}</h3>
             <p>Title:{title}</p>
-            <p><small>{body}</small></p>
-            
+            <p><small>{body}</small></p> 
+            <button onClick={handleGoback}>Go back</button>  
         </div>
     );
 };
